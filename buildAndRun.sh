22bc2f24fcec77b1f40c15430ae74de36441b99a
Andreas Falkenberg <andreas@falkenbergtech.com>
2026-04-10 22:44:12 -0700

    Update build script to include additional source files

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -3,11 +3,11 @@ wget https://raw.githubusercontent.com/yhirose/cpp-httplib/master/httplib.h
 wget https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp
 
 # Linux/macOS
-g++ -std=c++17 -O2 -pthread ftx1_server.cpp -o ftx1_server
+g++ -std=c++17 -O2 -pthread ftx1_server.cpp FTX1Controller.cpp FTX1HttpServer.cpp -o ftx1_server
 ./ftx1_server --port /dev/ttyUSB0 --baud 38400 --http 8080
 
 # Windows (MSVC)
-cl /std:c++17 /O2 /EHsc ftx1_server.cpp /link ws2_32.lib
+cl /std:c++17 /O2 /EHsc ftx1_server.cpp  FTX1Controller.cpp FTX1HttpServer.cpp /link ws2_32.lib
 ftx1_server.exe --port COM5 --baud 38400 --http 8080
 
 # Then open ftx1_gui.html in your browser
